24b1412dc969276ee46d2882c2015592dc327857
Paul Cowie <2082442C@student.gla.ac.uk>
2015-11-20 12:52:04 +0000

    Start working on map

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,21 @@
+var GOOGLE_MAPS_API_KEY = "AIzaSyDtHSNoJ5Ua1mHgdG_1pAX_RwBWvps_8ms";
+
 var get_parsed_data = function () {
     return Papa.parse(csv_data, {
         newline: "",
         header: true
     });
 };
+
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 55.8628, lng: 4.2542},
+    zoom: 8
+  });
+}
+
 
 $(document).ready(function(){
     // Example usage
