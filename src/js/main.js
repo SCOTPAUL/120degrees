d4b847202cc8fb59e304e1cf1e6bfe8189e18899
Paul Cowie <2082442C@student.gla.ac.uk>
2015-11-21 14:00:25 +0000

    Add automatic geolocation option and current location text

--- src/js/main.js
+++ src/js/main.js
@@ -9,56 +9,98 @@ var get_parsed_data = function () {
     });
 };
 
-function initMap() {
+var initMap = function() {
     var latlng = {lat: 55.8628, lng: -4.2542}
-    
+
     map = new google.maps.Map(document.getElementById('map'), {
         center: latlng,
-        zoom: 14
+        zoom: 12
     });
-    
+
     pos_marker = new google.maps.Marker({
         position: latlng,
         map: map,
         draggable: true,
         title: 'Location'
     });
+
+    var geocoder = new google.maps.Geocoder;
+    var set_location_text = function(){
+        reverseGeocodeMarker(geocoder, map, $("#location"));
+    };
+
+    google.maps.event.addListener(pos_marker, 'dragend', function(){
+        set_location_text();
+    });
+
+    var geolocation_permission = false;
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position){
+            latlng = {lat: position.coords.latitude, lng: position.coords.longitude};
+            pos_marker.setPosition(latlng);
+            map.setCenter(latlng);
+            set_location_text();
+            geolocation_permission = true;
+        });
+    }
+
+    if(!geolocation_permission){
+        set_location_text();
+    }
 }
 
 var toggle_map = function(){
-    map_div = $("#map");    
+    map_div = $("#map");
     var visibility = map_div.css("visibility");
 
     if (visibility == "hidden"){
         map_div.css("visibility","visible")
             .hide()
-            .slideDown("slow");        
-        $(".set-location").html("Set Location &#9650;");
+            .slideDown("slow");
+        $("#set-location-text").html("Set Location &#9650;");
     }
     else {
         map_div.slideUp("slow", function(){
             $(this).css("visibility", "hidden");
         });
-        $(".set-location").html("Set Location &#9660;");
+        $("#set-location-text").html("Set Location &#9660;");
     }
-}
-
+};
 
 var get_user_location = function(){
     return pos_marker.getPosition();
-}
+};
+
+var format_location_name = function(result){
+    if(result.address_components.length >= 3){
+        var component = result.address_components;
+        return component[0].short_name + " " + component[1].short_name + ", " + component[2].short_name;
+    }
+
+    return result.formatted_address;
+};
+
+var reverseGeocodeMarker = function(geocoder, map, textbox){
+    textbox.text("Finding Location...");
+
+    var pos = get_user_location();
+    geocoder.geocode({'location': pos}, function(results, status){
+        if(status == google.maps.GeocoderStatus.OK){
+            if(results[0]){
+                textbox.text(format_location_name(results[0]));
+            }
+        }
+    });
+
+};
+
 
 $(document).ready(function(){
     // Example usage
     var data = get_parsed_data().data;
     var i;
 
-    for(i = 0; i < data.length; ++i){
-        console.log(data[i].BusinessName);
-    }
-    
-    
     $(".set-location").click(function(){
         toggle_map();
-    });    
+    });
 });
