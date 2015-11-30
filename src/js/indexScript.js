46bb92b49f21a3876915307ac318ec938f23fd7a
Paul Cowie <2082442C@student.gla.ac.uk>
2015-11-30 14:33:07 +0000

    Can remove tags by dragging

--- src/js/indexScript.js
+++ src/js/indexScript.js
@@ -2,8 +2,7 @@ function drag_start(event){
     var style = window.getComputedStyle(event.target, null);
     var str = (parseInt(style.getPropertyValue("left")) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top")) - event.clientY)+ ',' + event.target.id;
     event.dataTransfer.setData("Text",str);
-};
-
+}
 function drop(event){
     var offset = event.dataTransfer.getData("Text").split(',');
     var dm = document.getElementById(offset[2]);
@@ -12,25 +11,29 @@ function drop(event){
 
     event.preventDefault();
     return false;
-};
-
+}
 function drag_over(event){
     event.preventDefault();
     return false;
-};
-
+}
 var tag_map = new Map();
 
 function getScore(elem){
     var p = $(elem);
+    var tag_text = p.text();
     var position = p.position();
     var $item = $(body);
-    var bodyOffset = $item.offset()
-    var bitToAdd = p.width()/2
+    var bodyOffset = $item.offset();
+    var bitToAdd = p.width()/2;
     var width1 = $item.width();
     var midpoint = (width1 +  bodyOffset.left )/ 2;
     var difference = (position.left + bitToAdd) - midpoint;
     var divPos = $("#pinboard").offset();
+    if(position.top + p.height() < divPos.top){
+        tag_map.delete(tag_text);
+        return null;
+    }
+
     var score = (difference / (midpoint - divPos.left)) * 1.2;
     if (score < -1) {
         score = -1;
@@ -39,16 +42,9 @@ function getScore(elem){
       score = 1;
     }
 
-    var tag_text = p.text();
     tag_map.set(tag_text, score);
-    $( "#text" ).append( "<br>id = " + elem.id + " || score: " + score);
-};
-
-function getWidth(elem){
-    var $this = $(body);
-    var width = $this.width();
-    $( "#text" ).text( "midpoint = " + width/2 );
-};
+    return score;
+}
 
 var tagId = 0;
 function createDTag(insertionPoint, label, colorClass){
@@ -61,14 +57,14 @@ function createDTag(insertionPoint, label, colorClass){
                             .addClass("dTag")
                             .text(label)
                             .on('dragstart', function(event){drag_start(event.originalEvent);})
-                            .on('dragend', function(event){getScore(this);});
+                            .on('dragend', function(){getScore(this);});
 
     if(colorClass){
         new_tag.addClass(colorClass);
     }
 
     ++tagId;
-};
+}
 
 function search(query){
     if($.inArray(query, all_cuisines) != -1){
